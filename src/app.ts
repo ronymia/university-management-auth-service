bd347d9d972711b9750bb126c81afb642115f929
MD Rony Mia <mdronymia.javascript@gmail.com>
2024-05-16 00:15:18 +0600

    users setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express();
 
 
@@ -9,9 +10,13 @@ app.use(cors())
 //parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+// Application routes
+
+app.use('/api/v1/users/', usersRouter)
+
 //Testing
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working Successfully");
-});
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Working Successfully')
+})
 
 export default app;
