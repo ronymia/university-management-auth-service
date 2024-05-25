7f9c048f67bebf1af67abfd356d7d133449f6273
MD Rony Mia <mdronymia.javascript@gmail.com>
2024-05-25 15:22:37 +0600

    handle CastError

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -6,6 +6,7 @@ import ApiError from '../../errors/ApiError';
 import { errorLogger } from '../../shared/logger';
 import { ZodError } from 'zod';
 import handleZodError from '../../errors/handleZodError';
+import handleCastError from '../../errors/handleCastError';
 
 const globalErrorHandler: ErrorRequestHandler = async (
     error,
@@ -28,6 +29,11 @@ const globalErrorHandler: ErrorRequestHandler = async (
         errorMessages = simplifiedError?.errorMessages;
     } else if (error instanceof ZodError) {
         const simplifiedError = handleZodError(error);
+        statusCode = simplifiedError?.statusCode;
+        message = simplifiedError?.message;
+        errorMessages = simplifiedError?.errorMessages;
+    } else if (error?.name === 'CastError') {
+        const simplifiedError = handleCastError(error);
         statusCode = simplifiedError?.statusCode;
         message = simplifiedError?.message;
         errorMessages = simplifiedError?.errorMessages;
---
+++ src/errors/handleCastError.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import { IGenericErrorResponse } from '../interfaces/common';
+import httpStatus from 'http-status-codes';
+import { IGenericErrorMessage } from '../interfaces/error';
+
+const handleCastError = (
+    error: mongoose.Error.CastError,
+): IGenericErrorResponse => {
+    const errors: IGenericErrorMessage[] = [
+        {
+            path: error.path,
+            message: `Invalid id - ${error.value}`,
+        },
+    ];
+
+    return {
+        statusCode: httpStatus.NOT_ACCEPTABLE,
+        message: 'Cast Error',
+        errorMessages: errors,
+    };
+};
+
+export default handleCastError;
