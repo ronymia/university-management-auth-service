e2ede1e1ab6417b41ee59357978291ace83eb313
MD Rony Mia <mdronymia.javascript@gmail.com>
2024-06-01 01:32:31 +0600

    faculty crud operation complete

---
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -0,0 +1,105 @@
+import { Request, Response } from 'express';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import httpStatus from 'http-status-codes';
+import { IAcademicFaculty } from './academicFaculty.interface';
+import { AcademicFacultyService } from './academicFaculty.service';
+import pick from '../../../shared/pick';
+import { academicFacultyFilterableFields } from './academicFaculty.constant';
+import { paginationFields } from '../../../constant/pagination';
+
+// create faculty
+const createAcademicFaculty = catchAsync(
+    async (req: Request, res: Response) => {
+        const { ...academicFacultyData } = req.body;
+        const result =
+            await AcademicFacultyService.createAcademicFaculty(
+                academicFacultyData,
+            );
+
+        sendResponse<IAcademicFaculty>(res, {
+            statusCode: httpStatus.CREATED,
+            success: true,
+            message: 'Academic Faculty created successfully!',
+            data: result,
+        });
+    },
+);
+
+// get single Faculty
+const getSingleAcademicFaculty = catchAsync(
+    async (req: Request, res: Response) => {
+        const { id } = req.params;
+        const result =
+            await AcademicFacultyService.getSingleAcademicFaculty(id);
+
+        sendResponse<IAcademicFaculty>(res, {
+            statusCode: httpStatus.OK,
+            success: true,
+            message: 'Academic Faculty fetched successfully!',
+            data: result,
+        });
+    },
+);
+
+// get all Faculty
+const getAllAcademicFaculty = catchAsync(
+    async (req: Request, res: Response) => {
+        const filters = pick(req.query, academicFacultyFilterableFields);
+        const paginationOptions = pick(req.query, paginationFields);
+
+        const result = await AcademicFacultyService.getAllAcademicFaculty(
+            filters,
+            paginationOptions,
+        );
+
+        sendResponse<IAcademicFaculty[]>(res, {
+            statusCode: httpStatus.OK,
+            success: true,
+            message: 'Academic Faculty fetched successfully!',
+            meta: result.meta,
+            data: result.data,
+        });
+    },
+);
+
+// update single Faculty
+const updateAcademicFaculty = catchAsync(
+    async (req: Request, res: Response) => {
+        const { id } = req.params;
+        const result = await AcademicFacultyService.updateAcademicFaculty(
+            id,
+            req.body,
+        );
+
+        sendResponse<IAcademicFaculty>(res, {
+            statusCode: httpStatus.OK,
+            success: true,
+            message: 'Academic Faculty updated successfully!',
+            data: result,
+        });
+    },
+);
+
+// delete single Faculty
+const deleteAcademicFaculty = catchAsync(
+    async (req: Request, res: Response) => {
+        const { id } = req.params;
+        const result = await AcademicFacultyService.deleteAcademicFaculty(id);
+
+        sendResponse<IAcademicFaculty>(res, {
+            statusCode: httpStatus.OK,
+            success: true,
+            message: 'Academic Faculty Deleted successfully!',
+            data: result,
+        });
+    },
+);
+
+export const AcademicFacultyController = {
+    createAcademicFaculty,
+    getSingleAcademicFaculty,
+    getAllAcademicFaculty,
+    updateAcademicFaculty,
+    deleteAcademicFaculty,
+};
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { UserRoutes } from '../modules/user/user.route';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 const router = express.Router();
 
 const moduleRoutes = [
@@ -11,6 +12,10 @@ const moduleRoutes = [
     {
         path: '/academic-semester/',
         route: AcademicSemesterRoutes,
+    },
+    {
+        path: '/academic-faculty/',
+        route: AcademicFacultyRoutes,
     },
 ];
 
