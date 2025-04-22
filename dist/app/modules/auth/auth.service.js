6327b3f2858e788b95a4a039858c39c051b9b319
ronymia <rony.mia7800@gmail.com>
2025-04-22 10:20:59 +0600

    feat: implement user module with CRUD operations and validation
    - Added user interface, model, service, and routes for handling user-related operations.
    - Implemented user creation for students, faculty, and admins with role-based access.
    - Integrated Zod for request validation and error handling.
    - Created utility functions for generating unique IDs for users.
    - Established a centralized configuration for environment variables.
    - Developed pagination and query builder helpers for efficient data retrieval.
    - Set up logging with Winston for better error tracking and monitoring.
    - Added error handling middleware for improved API response management.

---
+++ dist/app/modules/auth/auth.service.js
@@ -0,0 +1,117 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AuthService = void 0;
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const ApiError_1 = __importDefault(require("../../../errors/ApiError"));
+const user_model_1 = require("../user/user.model");
+const config_1 = __importDefault(require("../../../config"));
+const jwtHelpers_1 = require("../../../helpers/jwtHelpers");
+const http_status_1 = __importDefault(require("http-status"));
+const loginUser = (payload) => __awaiter(void 0, void 0, void 0, function* () {
+    const user = new user_model_1.User();
+    const { id, password } = payload;
+    const isUserExist = yield user.isUserExist(id);
+    if (!isUserExist) {
+        throw new ApiError_1.default(http_status_1.default.NOT_FOUND, ' User not found');
+    }
+    // check password
+    const isPasswordMatch = isUserExist.password &&
+        (yield user.isPasswordMatch(password, isUserExist.password));
+    if (!isPasswordMatch) {
+        throw new ApiError_1.default(http_status_1.default.UNAUTHORIZED, ' Password incorrect');
+    }
+    // create JWT token and refresh token
+    const { id: userId, role, needsChangePassword } = isUserExist;
+    const accessToken = jwtHelpers_1.jwtHelpers.createToken({ userId, role }, config_1.default.jwt.secret, config_1.default.jwt.expires_in);
+    const refreshToken = jwtHelpers_1.jwtHelpers.createToken({ userId, role }, config_1.default.jwt.refresh_secret, config_1.default.jwt.refresh_expires_in);
+    return {
+        accessToken,
+        refreshToken,
+        needsChangePassword,
+    };
+});
+const refreshToken = (token) => __awaiter(void 0, void 0, void 0, function* () {
+    // Invalid token - synchronous
+    let verifiedToken = null;
+    try {
+        verifiedToken = jwtHelpers_1.jwtHelpers.verifyToken(token, config_1.default.jwt.refresh_secret);
+    }
+    catch (err) {
+        // Log the error for debugging purposes
+        console.error('JWT verification error:', err);
+        // Differentiate between token expiration and other errors
+        if (err.name === 'TokenExpiredError') {
+            throw new ApiError_1.default(http_status_1.default.UNAUTHORIZED, 'JWT token has expired');
+        }
+        else if (err.name === 'JsonWebTokenError') {
+            throw new ApiError_1.default(http_status_1.default.FORBIDDEN, 'Invalid JWT token');
+        }
+        else {
+            throw new ApiError_1.default(http_status_1.default.FORBIDDEN, 'Could not verify JWT token');
+        }
+    }
+    const { userId } = verifiedToken;
+    const user = new user_model_1.User();
+    const isUserExist = yield user.isUserExist(userId);
+    if (!isUserExist) {
+        throw new ApiError_1.default(http_status_1.default.NOT_FOUND, ' User not found');
+    }
+    //generate new token
+    const newAccessToken = jwtHelpers_1.jwtHelpers.createToken({
+        id: isUserExist.id,
+        role: isUserExist.role,
+    }, config_1.default.jwt.secret, config_1.default.jwt.expires_in);
+    return {
+        accessToken: newAccessToken,
+    };
+});
+const changePassword = (user, payload) => __awaiter(void 0, void 0, void 0, function* () {
+    const { oldPassword, newPassword } = payload;
+    // // checking is user exist
+    // const isUserExist = await User.isUserExist(user?.userId);
+    //alternative way
+    const isUserExist = yield user_model_1.User.findOne({ id: user === null || user === void 0 ? void 0 : user.userId }).select('+password');
+    if (!isUserExist) {
+        throw new ApiError_1.default(http_status_1.default.NOT_FOUND, 'User does not exist');
+    }
+    // checking old password
+    const userExist = new user_model_1.User();
+    if (isUserExist.password &&
+        !(yield userExist.isPasswordMatch(oldPassword, isUserExist.password))) {
+        throw new ApiError_1.default(http_status_1.default.UNAUTHORIZED, 'Old Password is incorrect');
+    }
+    // // hash password before saving
+    // const newHashedPassword = await bcrypt.hash(
+    //   newPassword,
+    //   Number(config.bycrypt_salt_rounds)
+    // );
+    // const query = { id: user?.userId };
+    // const updatedData = {
+    //   password: newHashedPassword,  //
+    //   needsPasswordChange: false,
+    //   passwordChangedAt: new Date(), //
+    // };
+    // await User.findOneAndUpdate(query, updatedData);
+    // data update
+    isUserExist.password = newPassword;
+    isUserExist.needsChangePassword = false;
+    // updating using save()
+    isUserExist.save();
+});
+exports.AuthService = {
+    loginUser,
+    refreshToken,
+    changePassword,
+};
