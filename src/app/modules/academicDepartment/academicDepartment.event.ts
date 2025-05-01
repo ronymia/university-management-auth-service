12603152a9eb3f2969e52d507cffe35f0b06440e
ronymia <rony.mia7800@gmail.com>
2025-05-02 01:19:24 +0600

    feat: add event handling for academic departments and faculties with update and delete functionality

--- src/app/modules/academicDepartment/academicDepartment.event.ts
+++ src/app/modules/academicDepartment/academicDepartment.event.ts
@@ -1,15 +1,46 @@
 import { RedisClient } from '../../../shared/redis';
-import { EVENT_ACADEMIC_DEPARTMENT_CREATED } from './academicDepartment.constant';
+import {
+    EVENT_ACADEMIC_DEPARTMENT_CREATED,
+    EVENT_ACADEMIC_DEPARTMENT_DELETED,
+    EVENT_ACADEMIC_DEPARTMENT_UPDATED,
+} from './academicDepartment.constant';
 import { AcademicDepartmentService } from './academicDepartment.service';
 
 const initAcademicDepartmentEvents = async () => {
-    RedisClient.subscribe(EVENT_ACADEMIC_DEPARTMENT_CREATED, async (e: any) => {
-        const data = JSON.parse(e);
-        await AcademicDepartmentService.createAcademicDepartmentFromEvent({
-            ...data,
-            syncId: data?.id,
-        });
-    });
+    // CREATE ACADEMIC DEPARTMENT
+    RedisClient.subscribe(
+        EVENT_ACADEMIC_DEPARTMENT_CREATED,
+        async (e: string) => {
+            const data = JSON.parse(e);
+            await AcademicDepartmentService.createAcademicDepartmentFromEvent({
+                ...data,
+                syncId: data?.id,
+            });
+        },
+    );
+
+    // UPDATE ACADEMIC DEPARTMENT
+    RedisClient.subscribe(
+        EVENT_ACADEMIC_DEPARTMENT_UPDATED,
+        async (e: string) => {
+            const data = JSON.parse(e);
+            await AcademicDepartmentService.updateAcademicDepartmentFromEvent({
+                ...data,
+                syncId: data?.id,
+            });
+        },
+    );
+
+    // DELETE ACADEMIC DEPARTMENT
+    RedisClient.subscribe(
+        EVENT_ACADEMIC_DEPARTMENT_DELETED,
+        async (e: string) => {
+            const data = JSON.parse(e);
+            await AcademicDepartmentService.deleteAcademicDepartmentFromEvent(
+                data?.id,
+            );
+        },
+    );
 };
 
 // EXPORT EVENTS
--- src/app/modules/academicFaculty/academicFaculty.event.ts
+++ src/app/modules/academicFaculty/academicFaculty.event.ts
@@ -1,8 +1,13 @@
 import { RedisClient } from '../../../shared/redis';
-import { EVENT_ACADEMIC_FACULTY_CREATED } from './academicFaculty.constant';
+import {
+    EVENT_ACADEMIC_FACULTY_CREATED,
+    EVENT_ACADEMIC_FACULTY_DELETED,
+    EVENT_ACADEMIC_FACULTY_UPDATED,
+} from './academicFaculty.constant';
 import { AcademicFacultyService } from './academicFaculty.service';
 
 const initAcademicFaculty = async () => {
+    // CREATE ACADEMIC FACULTY
     await RedisClient.subscribe(
         EVENT_ACADEMIC_FACULTY_CREATED,
         async (e: any) => {
@@ -11,6 +16,29 @@ const initAcademicFaculty = async () => {
                 ...data,
                 syncId: data?.id,
             });
+        },
+    );
+
+    // UPDATE ACADEMIC FACULTY
+    await RedisClient.subscribe(
+        EVENT_ACADEMIC_FACULTY_UPDATED,
+        async (e: any) => {
+            const data = JSON.parse(e);
+            await AcademicFacultyService.updateAcademicFacultyFromEvent({
+                ...data,
+                syncId: data?.id,
+            });
+        },
+    );
+
+    // DELETE ACADEMIC FACULTY
+    await RedisClient.subscribe(
+        EVENT_ACADEMIC_FACULTY_DELETED,
+        async (e: any) => {
+            const data = JSON.parse(e);
+            await AcademicFacultyService.deleteAcademicFacultyFromEvent(
+                data.id,
+            );
         },
     );
 };
