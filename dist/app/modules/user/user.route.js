0844408ec8b681a185c0032ee10d2da81b1e3a7e
MD Rony Mia <rony.mia7800@gmail.com>
2025-06-09 20:55:24 +0600

    feat: replace validation middleware with role-based authentication for user creation routes

--- dist/app/modules/user/user.route.js
+++ dist/app/modules/user/user.route.js
@@ -6,12 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserRoutes = void 0;
 const express_1 = __importDefault(require("express"));
 const user_controller_1 = require("./user.controller");
-const validateRequest_1 = __importDefault(require("../../middlewares/validateRequest"));
-const user_validation_1 = require("./user.validation");
+const auth_1 = __importDefault(require("../../middlewares/auth"));
+const user_1 = require("../../../enums/user");
 const router = express_1.default.Router();
 // Student
-router.post('/create-student', (0, validateRequest_1.default)(user_validation_1.UserValidation.createStudentZodSchema), user_controller_1.UserController.createStudent);
+router.post('/create-student', (0, auth_1.default)(user_1.ENUM_USER_ROLE.SUPER_ADMIN, user_1.ENUM_USER_ROLE.ADMIN), 
+// validateRequest(UserValidation.createStudentZodSchema),
+user_controller_1.UserController.createStudent);
 // Faculty
-router.post('/create-faculty', (0, validateRequest_1.default)(user_validation_1.UserValidation.createFacultyZodSchema), user_controller_1.UserController.createFaculty);
-router.post('/create-admin', (0, validateRequest_1.default)(user_validation_1.UserValidation.createAdminZodSchema), user_controller_1.UserController.createAdmin);
+router.post('/create-faculty', (0, auth_1.default)(user_1.ENUM_USER_ROLE.SUPER_ADMIN, user_1.ENUM_USER_ROLE.ADMIN), 
+// validateRequest(UserValidation.createFacultyZodSchema),
+user_controller_1.UserController.createFaculty);
+router.post('/create-admin', (0, auth_1.default)(user_1.ENUM_USER_ROLE.SUPER_ADMIN, user_1.ENUM_USER_ROLE.ADMIN), 
+// validateRequest(UserValidation.createAdminZodSchema),
+user_controller_1.UserController.createAdmin);
 exports.UserRoutes = router;
